216e3099ae7242d2b00ddc1abfff357c730db1b0
Dawid Janczak <dawid.janczak@gmail.com>
2021-05-03 12:35:38 +0800

    Center and restrict map on Taiwan

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,19 @@ import 'leaflet/dist/leaflet.css'
 import L from 'leaflet'
 import './index.css'
 
-const map = L.map('map').setView([51.505, -0.09], 13)
+const northWest = L.latLng(26, 117)
+const southEast = L.latLng(21.5, 124)
+
+const map = L.map(
+  'map',
+  {
+    center: L.latLng(23.6, 120.965),
+    zoom: 8,
+    maxBounds: L.latLngBounds(northWest, southEast),
+    minZoom: 8,
+    preferCanvas: true
+  }
+)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map)
-console.log('foo')
