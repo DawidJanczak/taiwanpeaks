79d2b43ca4ddce1fae0801d424f0f4a850fc3bf2
Dawid Janczak <dawid.janczak@gmail.com>
2021-05-29 12:42:58 +0800

    Add popup for Yushan

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import 'leaflet/dist/leaflet.css'
 import 'mapbox-gl/dist/mapbox-gl.css'
-import L from 'leaflet'
 import mapboxgl from 'mapbox-gl'
+import type GeoJSON from 'geojson'
 import './index.css'
 
 // const northWest = L.latLng(26, 117)
@@ -27,4 +27,58 @@ const map = new mapboxgl.Map({
   center: [120.5, 23.6],
   container: 'map',
   style: 'mapbox://styles/mapbox/outdoors-v11'
+})
+
+map.on('load', () => {
+  map.addSource('top100', {
+    type: 'geojson',
+    data: {
+      type: 'FeatureCollection',
+      features: [
+        {
+          type: 'Feature',
+          properties: {
+            description: 'Yushan'
+          },
+          geometry: {
+            type: 'Point',
+            coordinates: [120.957, 23.47]
+          }
+        }
+      ]
+    }
+  })
+  map.addLayer({
+    id: 'top100',
+    type: 'circle',
+    source: 'top100',
+    paint: {
+      'circle-color': '#4264fb',
+      'circle-radius': 6,
+      'circle-stroke-width': 2,
+      'circle-stroke-color': '#ffffff'
+    }
+  })
+
+  const popup = new mapboxgl.Popup({
+    closeButton: false,
+    closeOnClick: false
+  })
+
+  map.on('mouseenter', 'top100', (e) => {
+    map.getCanvas().style.cursor = 'pointer';
+
+    if (e.features && e.features[0]) {
+      const feature = e.features[0];
+      const coordinates = (<GeoJSON.Point>feature.geometry).coordinates.slice() as mapboxgl.LngLatLike;
+      const description = feature.properties?.description;
+
+      popup.setLngLat(coordinates).setHTML(description).addTo(map);
+    }
+  })
+
+  map.on('mouseleave', 'top100', () => {
+    map.getCanvas().style.cursor = '';
+    popup.remove();
+  })
 })
